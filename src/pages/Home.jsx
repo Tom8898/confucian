e7071816a4e6f1160ab621a61e12052cb1ff85c3
Tom8898 <jingtao416@gmail.com>
2025-07-30 15:24:04 +1200

    logo picture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="border-8 border-blue-400 min-h-screen bg-gray-50">
+    <div className="border-1 border-blue-400 min-h-screen bg-gray-50">
       {/* Navbar */}
       <nav className="bg-white shadow">
         <div className="border border-gray-400 max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
@@ -27,8 +27,8 @@ export default function Home() {
         </div>
       </nav>
 
-      {/* Hero Section */}
-      <section className="border border-gray-400 min-h-[400px] lg:h-[500px] bg-blue-100 text-center flex flex-col justify-center items-center">
+      {/*  Section */}
+      {/* <section className="border border-gray-400 min-h-[400px] lg:h-[500px] bg-blue-100 text-center flex flex-col justify-center items-center">
         <h2 className="text-4xl font-bold text-blue-800 mb-4">Welcome to New Zealand!</h2>
         <p className="text-lg text-blue-700 max-w-xl mx-auto">
           Your one-stop guide to settling, living, and thriving in Aotearoa.
@@ -37,6 +37,36 @@ export default function Home() {
           <Link to="/arrival" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
             Get Started
           </Link>
+        </div>
+      </section> */}
+
+      {/* <section className="min-h-[400px] lg:h-[500px] text-white flex flex-col justify-center items-center bg-cover bg-center animate-pulse"
+                style={{ backgroundImage: "url('/images/auckland-night.png')" }} >
+        <h2 className="text-4xl font-bold text-blue-800 mb-4">Welcome to New Zealand!</h2>
+        <p className="text-lg text-blue-700 max-w-xl mx-auto">
+          Your one-stop guide to settling, living, and thriving in Aotearoa.
+        </p>
+        <div className="mt-8">
+          <Link to="/arrival" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
+            Get Started
+          </Link>
+        </div>  
+      </section> */}
+
+
+      <section className="relative min-h-[400px] lg:h-[500px] text-white flex flex-col justify-center items-center bg-cover bg-center"
+               style={{ backgroundImage: "url('/images/auckland-night2.png')" }}>
+        <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-50"></div>
+        <div className="relative z-10 text-center">
+          <h2 className="text-4xl font-bold text-white-800 mb-4">Welcome to New Zealand!</h2>
+          <p className="text-lg text-white-700 max-w-xl mx-auto">
+            Your one-stop guide to settling, living, and thriving in Aotearoa.
+          </p>
+          <div className="mt-8">
+            <Link to="/arrival" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
+              Get Started
+            </Link>
+          </div>
         </div>
       </section>
 
